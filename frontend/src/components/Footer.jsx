491cc6ca0e1ed141bf89f1cbf8d3712da6cdceb2
Devik Gupta <64042688+guptadevik2003@users.noreply.github.com>
2024-06-12 22:18:12 +0530

    Create Footer.jsx

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,37 @@
+import { useNavigate } from 'react-router-dom';
+
+export default function Footer() {
+  const navigate = useNavigate();
+
+  function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+  return (
+    <footer className='bg-screenbglight pt-12 pb-24'>
+      <div className='w-[92%] mx-auto'>
+        <div className='flex md:flex-row flex-col'>
+
+          <div className="basis-2/5">
+            <button onClick={() => { navigate('/'); scrollToTop() }} className="cursor-pointer text-white text-5xl font-semibold">CertifyPro</button>
+            <h2 className="text-[#999] text-sm my-6">Effortless Certificate Generation for Clubs and Organizations.</h2>
+            <p className="text-[#666] text-sm">Copyright © 2024 Devik Gupta. All rights reserved.</p>
+          </div>
+
+          <div className="basis-1/5">
+            <h3 className="text-white mb-6 mt-6 md:mt-0 font-medium">Navigation</h3>
+            <button onClick={() => { navigate('/generator'); scrollToTop() }} className="text-[#999] hover:text-[#ddd] text-sm mb-[10px] block">Generator</button>
+          </div>
+
+          <div className="basis-1/5">
+            <h3 className="text-white mb-6 mt-6 md:mt-0 font-medium">Resources</h3>
+            <button onClick={() => { window.open('https://www.flaticon.com/authors/freepik', '_blank') }} className="text-[#999] hover:text-[#ddd] text-sm mb-[10px] block">Freepik</button>
+            <button onClick={() => { window.open('https://www.flaticon.com', '_blank') }} className="text-[#999] hover:text-[#ddd] text-sm block">Flaticon</button>
+          </div>
+
+        </div>
+      </div>
+    </footer>
+  );
+}
