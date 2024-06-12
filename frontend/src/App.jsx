1bec90511081675a033a5962f0699c9d1efc4ebb
Devik Gupta <64042688+guptadevik2003@users.noreply.github.com>
2024-06-12 17:42:36 +0530

    Updated base app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // Pages - Root
 import Home from 'pages/Root/Home';
 import PageNotFound from 'pages/Root/PageNotFound';
+import Generator from 'pages/Root/Generator';
 
 // Components
-
+import Navbar from 'components/Navbar';
+import Footer from 'components/Footer';
 
 export default function App() {
   return (
@@ -15,9 +17,11 @@ export default function App() {
 
         <Route path='/'>
 
-          <Route path='' element={<><Home /></>} />
+          <Route path='' element={<><Navbar /><Home /><Footer /></>} />
 
-          <Route path='*' element={<><PageNotFound /></>} />
+          <Route path='generator' element={<><Navbar /><Generator /><Footer /></>} />
+
+          <Route path='*' element={<><Navbar /><PageNotFound /><Footer /></>} />
 
         </Route>
 
