1248c7374d2242b7b87a94ebb234305740e53740
Devik Gupta <64042688+guptadevik2003@users.noreply.github.com>
2024-06-12 17:45:00 +0530

    Update PageNotFound.jsx

--- frontend/src/pages/Root/PageNotFound.jsx
+++ frontend/src/pages/Root/PageNotFound.jsx
@@ -1,7 +1,20 @@
+/* eslint-disable react/no-unescaped-entities */
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 export default function PageNotFound() {
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    document.title = 'Page Not Found - CertifyPro';
+  }, []);
+
   return (
-    <div>
-      <h1>Page Not Found!</h1>
+    <div className="flex flex-col items-center w-[92%] h-[80vh] justify-center mx-auto pb-[30px]">
+      <img className="md:w-[30%] w-[80%]" src="/page-not-found.svg"/>
+      <h1 className="text-white text-[12px] md:text-[16px]">The page you are looking for might have been removed,</h1>
+      <h1 className="text-white text-[12px] md:text-[16px]">had it's name changed or is temporarily unavailable.</h1>
+      <button onClick={() => navigate('/')} className="bg-blurple text-white font-medium px-5 py-2 rounded-lg hover:bg-transparent hover:text-blurple border border-blurple mt-[40px]">GO TO HOMEPAGE</button>
     </div>
   );
 }
