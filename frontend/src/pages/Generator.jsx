057ce20f1054a9f076b7b62de3feb9194fe9e5fe
Devik Gupta <64042688+guptadevik2003@users.noreply.github.com>
2024-06-20 02:34:53 +0530

    Certificate 0 Done

--- frontend/src/pages/Generator.jsx
+++ frontend/src/pages/Generator.jsx
@@ -183,7 +183,7 @@ export default function Generator() {
               <p className="text-[#999] mt-1 mb-4">Our tool offers a wide range of templates to use as a base for designing your certificate.</p>
 
               <div className="grid md:grid-cols-3 gap-3">
-                <img className="w-full border-4 border-transparent cursor-pointer template template-0" onClick={() => changeTemplate(0)} src="certificate-demo-0.jpeg" />
+                <img className="w-full border-4 border-transparent cursor-pointer template template-0" onClick={() => changeTemplate(0)} src="certificate-demo-0.png" />
                 <img className="w-full border-4 border-transparent cursor-pointer template template-1" onClick={() => changeTemplate(1)} src="certificate-demo-1.jpeg" />
                 <img className="w-full border-4 border-transparent cursor-pointer template template-2" onClick={() => changeTemplate(2)} src="certificate-demo-2.jpeg" />
                 <img className="w-full border-4 border-transparent cursor-pointer template template-3" onClick={() => changeTemplate(3)} src="certificate-demo-3.jpeg" />
